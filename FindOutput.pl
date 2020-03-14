0c408f0b2f0624e80d0e5b96dc725ce546dde9c8
cdxing <danielxchenx@gmail.com>
2020-03-14 11:23:24 -0400

    \n

--- FindOutput.pl
+++ FindOutput.pl
@@ -81,7 +81,7 @@ foreach my $i (keys %missingnum)
       if($missingnum{$i}==0)
       {
   	   print "$i ";#$missingJobid{$i}\n";
-       print "star-submit-template -template submitPicoDstJobs1.xml -entities myFileName=entryRange_$i,entryTestPar1=$i";
+       print "star-submit-template -template submitPicoDstJobs1.xml -entities myFileName=entryRange_$i,entryTestPar1=$i\n";
   	   #system ("cd ${dir_to_open_2}/") or die "could not execute $!\n";
   	   #my $out = `cd ${dir_to_open_2}`;
 
