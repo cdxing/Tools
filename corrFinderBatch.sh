9b206da1118068a2654eef95ef9ba3b3fa997e20
cdxing <danielxchenx@gmail.com>
2020-10-10 10:29:54 -0400

    resubmit jobs

--- corrFinderBatch.sh
+++ corrFinderBatch.sh
@@ -35,7 +35,7 @@ root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_dedx_var1_iter2_.root",5,1
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dedx_var2_iter2_
 root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_dedx_var2_iter2_.root",5,2,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var1_iter2_
-root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_dipAngle_var1_iter2_.root",12,1,2)'
+# root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_dipAngle_var1_iter2_.root",12,1,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaGap_var1_iter2_
 root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_etaGap_var1_iter2_.root",1,1,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaGap_var2_iter2_
@@ -45,23 +45,23 @@ root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_etaRange_var1_iter2_.root"
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var2_iter2_
 root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_etaRange_var2_iter2_.root",2,2,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_mass2_var1_iter2_
-root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_mass2_var1_iter2_.root",10,1,2)'
+# root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_mass2_var1_iter2_.root",10,1,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_mass2_var2_iter2_
-root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_mass2_var2_iter2_.root",10,2,2)'
+# root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_mass2_var2_iter2_.root",10,2,2)'
 # resubmitJobs.pl EpCorrection_OUTPUT_sys_nHitsFit_var1_iter2_
 root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_nHitsFit_var1_iter2_.root",7,1,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_nHitsFit_var2_iter2_
 root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_nHitsFit_var2_iter2_.root",7,2,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_nSigK_var1_iter2_
-root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_nSigK_var1_iter2_.root",9,1,2)'
+# root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_nSigK_var1_iter2_.root",9,1,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_nSigK_var2_iter2_
-root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_nSigK_var2_iter2_.root",9,2,2)'
+# root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_nSigK_var2_iter2_.root",9,2,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_primary_var0_iter2_
 root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_primary_var0_iter2_.root",0,0,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_pT_var1_iter2_
-root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_pT_var1_iter2_.root",11,1,2)'
+# root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_pT_var1_iter2_.root",11,1,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_pT_var2_iter2_
-root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_pT_var2_iter2_.root",11,2,2)'
+# root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_pT_var2_iter2_.root",11,2,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_ratio_var1_iter2_
 root -q -b -l corrFinder.cxx'("EpCorrection_INPUT_sys_ratio_var1_iter2_.root",8,1,2)'
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_ratio_var2_iter2_
