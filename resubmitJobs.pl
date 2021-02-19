d82a181967107334b222e318654ea6857f6fec10
cdxing <danielxchenx@gmail.com>
2021-02-18 23:33:30 -0500

    8441 resubmit

--- resubmitJobs.pl
+++ resubmitJobs.pl
@@ -77,7 +77,7 @@ my @sorted = sort{$a <=> $b} @unsorted;
 
 my %missingnum;
 # a hash of missing numbers
-for(my $i=0; $i<=844; $i++) # modify the total # of Jobs here
+for(my $i=0; $i<=8441; $i++) # modify the total # of Jobs here
 {
     $missingnum{$i} = 0;
 }
