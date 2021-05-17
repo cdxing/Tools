bdabde8b46a8902398243c655c5ac32be923de99
cdxing <danielxchenx@gmail.com>
2021-05-17 03:40:44 -0400

    merge.sh

---
+++ merge.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+./Merge.csh EpCorrection_OUTPUT_sys_dca_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_dca_var2_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_dedx_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_dedx_var2_iter1
+# ./Merge.csh EpCorrection_OUTPUT_sys_dipAngle_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_etaGap_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_etaGap_var2_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_etaRange_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_etaRange_var2_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_etaRange_var3_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_mass2_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_mass2_var2_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_nHitsFit_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_nHitsFit_var2_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_nSigK_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_nSigK_var2_iter1
+# ./Merge.csh EpCorrection_OUTPUT_sys_primary_var0_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_pT_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_pT_var2_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_ratio_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_ratio_var2_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_vr_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_vr_var2_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_vz_var1_iter1
+./Merge.csh EpCorrection_OUTPUT_sys_vz_var2_iter1
