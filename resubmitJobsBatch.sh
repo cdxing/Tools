22f412937ad2404d81df385d8801345cb0411c1c
cdxing <danielxchenx@gmail.com>
2021-05-19 14:46:03 -0400

    resubmit iter3

--- resubmitJobsBatch.sh
+++ resubmitJobsBatch.sh
@@ -8,37 +8,37 @@
 # date: 9/22/2020
 #
 ######################################################
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_dca_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_dca_var2_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_dedx_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_dedx_var2_iter2
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaGap_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaGap_var2_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var2_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var3_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_mass2_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_mass2_var2_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_nHitsFit_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_nHitsFit_var2_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_nSigK_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_nSigK_var2_iter2
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_primary_var0_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_pT_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_pT_var2_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_ratio_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_ratio_var2_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_vr_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_vr_var2_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_vz_var1_iter2
-./resubmitJobs.pl EpCorrection_OUTPUT_sys_vz_var2_iter2
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var0_iter2
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var1_iter2
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var2_iter2
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_dca_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_dca_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_dedx_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_dedx_var2_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaGap_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaGap_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var3_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_mass2_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_mass2_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_nHitsFit_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_nHitsFit_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_nSigK_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_nSigK_var2_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_primary_var0_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_pT_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_pT_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_ratio_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_ratio_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_vr_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_vr_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_vz_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_vz_var2_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var0_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var1_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var2_iter3
 
 # Additonal sys check for dip angle and eta Range
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var2_iter2
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var3_iter2
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var3_iter2
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var4_iter2
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var2_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var3_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var3_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var4_iter3
