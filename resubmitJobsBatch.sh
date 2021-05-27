e6ad562188223c80af9ccd2c1fd508e22619a88d
cdxing <danielxchenx@gmail.com>
2021-05-27 14:27:02 -0400

    iter'4'

--- resubmitJobsBatch.sh
+++ resubmitJobsBatch.sh
@@ -12,7 +12,6 @@
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dca_var2_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dedx_var1_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dedx_var2_iter3
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var1_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaGap_var1_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaGap_var2_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var1_iter3
@@ -24,7 +23,6 @@
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_nHitsFit_var2_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_nSigK_var1_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_nSigK_var2_iter3
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_primary_var0_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_pT_var1_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_pT_var2_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_ratio_var1_iter3
@@ -33,12 +31,15 @@
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_vr_var2_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_vz_var1_iter3
 ./resubmitJobs.pl EpCorrection_OUTPUT_sys_vz_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var1_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var2_iter3
+./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var3_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_primary_var0_iter3
+# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var1_iter3
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var0_iter3
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var1_iter3
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_TPCpid_var2_iter3
 
 # Additonal sys check for dip angle and eta Range
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var2_iter3
-# ./resubmitJobs.pl EpCorrection_OUTPUT_sys_dipAngle_var3_iter3
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var3_iter3
 # ./resubmitJobs.pl EpCorrection_OUTPUT_sys_etaRange_var4_iter3
